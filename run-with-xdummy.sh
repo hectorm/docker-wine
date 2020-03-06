2908d3c84b7c22457519f61878296e4373724e08
Héctor Molinero Fernández <hector@molinero.dev>
2020-03-06 20:20:11 +0100

    Applied changes from the base image

--- run-with-xdummy.sh
+++ run-with-xdummy.sh
@@ -41,7 +41,7 @@ printf -- '%s\n' "Creating \"${CONTAINER_NAME:?}\" container..."
 	--shm-size 2g \
 	--publish 3322:3322/tcp \
 	--publish 3389:3389/tcp \
-	--env ENABLE_VIRTUALGL=true \
+	--env ENABLE_XDUMMY=true \
 	--device /dev/dri:/dev/dri \
 	--mount type=volume,src="${VOLUME_NAME:?}",dst=/home/wine/ \
 	"${IMAGE_NAME:?}" "$@" >/dev/null
